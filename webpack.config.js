c4d2fbb28ae7111480556c30faef631bef5526e7
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-08-29 14:07:50 -0500

    Changes

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,13 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(mp3)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
     ],
   },
   devtool: 'source-map',
